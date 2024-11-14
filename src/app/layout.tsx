7ee1a43d1cc9f85244710487f3e311e0626d336e
AtnafOli <atinafabebe2@gmail.com>
2024-11-14 11:42:21 +0300

    fixed supsense issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CustomQueryClientProvider } from "@/components/providers/queryclient.providers";
 import Navbar from "@/components/common/navbar/navbar";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -100,15 +101,17 @@ export default function RootLayout({
       >
         <div className="">
           <main className="">
-            <UserProvider>
-              <TooltipProvider>
-                <div className="mx-auto w-full max-w-[1580px]">
-                  <CustomQueryClientProvider>
-                    {children}
-                  </CustomQueryClientProvider>
-                </div>
-              </TooltipProvider>
-            </UserProvider>
+            <Suspense fallback={<div>Loading...</div>}>
+              <UserProvider>
+                <TooltipProvider>
+                  <div className="mx-auto w-full max-w-[1580px]">
+                    <CustomQueryClientProvider>
+                      {children}
+                    </CustomQueryClientProvider>
+                  </div>
+                </TooltipProvider>
+              </UserProvider>
+            </Suspense>
           </main>
         </div>
       </body>
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   apiBaseUrl:
-    // process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8005/api",
-    "https://nice-events-api.onrender.com",
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8005/api",
+  // "https://nice-events-api.onrender.com",
   apiTimeout: 10000,
 };
