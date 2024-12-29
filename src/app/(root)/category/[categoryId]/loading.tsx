4236758ccd0ad6d99194f2e2ef8eea4e31b51f89
AtnafOli <atinafabebe2@gmail.com>
2024-12-29 14:57:39 +0300

    loading animation color change

--- src/app/(root)/category/[categoryId]/loading.tsx
+++ src/app/(root)/category/[categoryId]/loading.tsx
@@ -2,12 +2,12 @@ export default function LoadingServices() {
   return (
     <div className="min-h-screen bg-transparent py-12 px-4 sm:px-6 lg:px-16">
       <div className="animate-pulse">
-        <div className="h-8 bg-gray-400 rounded w-1/4 mb-4"></div>
-        <div className="h-12 bg-gray-400 rounded w-1/2 mb-8"></div>
+        <div className="h-8 bg-gray-300 rounded w-1/4 mb-4"></div>
+        <div className="h-12 bg-gray-300 rounded w-1/2 mb-8"></div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {[...Array(8)].map((_, i) => (
-            <div key={i} className="bg-gray-400 h-64 rounded-lg"></div>
+            <div key={i} className="bg-gray-300 h-64 rounded-lg"></div>
           ))}
         </div>
       </div>
